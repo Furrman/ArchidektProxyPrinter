612e0762c9a90e301283c9bb381cc51594f7bc9f
Furmi <16333282+Furrman@users.noreply.github.com>
2024-04-06 11:44:43 +0100

    Add code to download pictures from file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DownloadMTGCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ScryfallApiClient();
+            var filename = args[0];
+            var outputPath = args[1];
+            client.DownloadCards(filename, outputPath).Wait();
+        }
+    }
+}
