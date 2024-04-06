2bf048bba47ccc9803641562322ee528ae2c4b9f
Furmi <16333282+Furrman@users.noreply.github.com>
2024-04-06 11:44:44 +0100

    Add class to load card list straight from archidekt

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,13 +6,15 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var client = new ScryfallApiClient();
+        var archidektClient = new ArchidektApiClient();
+        var scryFallClient = new ScryfallApiClient();
         var wordGenerator = new PrintableCardsWordGenerator();
 
         var cardListFilename = args[0];
         var imageFolderPath = args[1];
 
-        client.DownloadCards(cardListFilename, imageFolderPath).Wait();
+        //var cardList = archidektClient.GetCardList("").Result;
+        scryFallClient.DownloadCards(cardListFilename, imageFolderPath).Wait();
         wordGenerator.GenerateWord(imageFolderPath, Path.Combine(imageFolderPath, cardListFilename));
     }
 }
