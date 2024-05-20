5ab7aecb868cffea5d339d9457bf60b4f6b15d44
Furmi <16333282+Furrman@users.noreply.github.com>
2024-05-20 17:39:06 +0100

    Add nlog.config to application assemblies

--- ConsoleApp/Configuration/NLogConfigurator.cs
+++ ConsoleApp/Configuration/NLogConfigurator.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using System.Xml;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Config;
 using NLog.Extensions.Logging;
@@ -9,15 +11,24 @@ internal static class NLogConfigurator
 {
     public static IServiceCollection SetupNLog(this IServiceCollection serviceCollection)
     {
-        string executablePath = AppDomain.CurrentDomain.BaseDirectory;
-        string configFilePath = Path.Combine(executablePath, "nlog.config");
-        var nlogConfig = new XmlLoggingConfiguration(configFilePath);
-        
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith("nlog.config"));
+
+        string configContent = string.Empty;
+        using Stream? stream = assembly.GetManifestResourceStream(resourceName);
+        if (stream is not null)
+        {
+            using StreamReader reader = new(stream!);
+            configContent = reader.ReadToEnd();
+        }
+
+        var nlogConfig = new XmlLoggingConfiguration(XmlReader.Create(new StringReader(configContent)));
+
         return serviceCollection
             .AddLogging(builder =>
             {
                 builder.ClearProviders();
-                builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Warning);
+                builder.SetMinimumLevel(LogLevel.Warning);
                 builder.AddNLog(nlogConfig, new NLogProviderOptions
                 {
                     CaptureMessageProperties = true,
